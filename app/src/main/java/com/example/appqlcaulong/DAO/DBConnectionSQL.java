6db2171c202b3527c7744136de888aa9d6324c42
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-02-01 21:22:27 +0700

    .

--- app/src/main/java/com/example/appqlcaulong/DAO/DBConnectionSQL.java
+++ app/src/main/java/com/example/appqlcaulong/DAO/DBConnectionSQL.java
@@ -4,26 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnectionSQL {
-//    private final String serverName = "DESKTOP-8C0E5N3";
-//    private final String dbName = "FinalWeb";
-//    private final String portNumber = "1433";
-//    private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
-//    private final String userID = "sa";
-//    private final String password = "nnmvpqh<302";
-//
-//    public Connection getConnection() throws Exception {
-//        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
-//        if (instance == null || instance.trim().isEmpty())
-//            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
-//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//        return DriverManager.getConnection(url, userID, password);
-//    }
-//
-//    public static void main(String[] args) {
-//        try {
-//            System.out.println(new DBConnectionSQL().getConnection());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private final String serverName = "DESKTOP-8C0E5N3";
+    private final String dbName = "FinalWeb";
+    private final String portNumber = "1433";
+    private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
+    private final String userID = "sa";
+    private final String password = "nnmvpqh<302";
+
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        if (instance == null || instance.trim().isEmpty())
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(new DBConnectionSQL().getConnection());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
